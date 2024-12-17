5a2132c8611b14e43e3db7c21d0c7c578ec1a9eb
Gaston <gaston@tdil.com.ar>
2024-12-17 15:56:21 -0300

    script -> using the key value of the json, I decompose the key into question level and options letter

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// Espera a que el documento esté listo
+// vairables
+let currentQuestion = 1;
+const moviesArray = {};
 // funciona igual que un fech a una API
 document.addEventListener("DOMContentLoaded", () => {
   // Cargar el archivo JSON que contiene los datos de las películas
   fetch("movies.json")
     .then((response) => response.json()) // Convertir la respuesta en formato JSON
     .then((data) => {
+      // Guardar los datos en la variable data
+      Object.assign(moviesArray, data);
       // Inicializar las imágenes en el HTML con los datos del JSON
       // initializeMovies(data);
-      formQuestions("quest1", data);
+      formQuestions("quest1", "A");
       updateQuestion(1);
     })
     .catch((error) => console.error("Error loading the movie data:", error));
@@ -23,30 +27,43 @@ function updateQuestion(current) {
 }
 
 // Funcion para inicializar progress questions
-function formQuestions(questions, data) {
+function formQuestions(questions, leterOption) {
+  console.log("formQuestions ->", questions, moviesArray, leterOption);
+  // Obtener las preguntas por su ID
+  const question1 = document.getElementById("question1");
+  // Obtener las preguntas por su ID
+  const question2 = document.getElementById("question2");
+  // Obtener las preguntas por su ID
+  const question3 = document.getElementById("question3");
+
   // switch case para cada pregunta
   switch (questions) {
     case "quest1":
-      // Obtener las preguntas por su ID
-      const question1 = document.getElementById("question1");
       // Le agrego la class active
       question1.classList.add("active");
       // Cargo las preguntas/ movies
-      optionsMovie(questions, data);
+      optionsMovie(questions, moviesArray, leterOption);
+      console.log("quest 1 active");
+
       break;
     case "quest2":
-      // Obtener las preguntas por su ID
-      const question2 = document.getElementById("question2");
+      // Le quito la class active
+      question1.classList.remove("active");
       // Le agrego la class active
       question2.classList.add("active");
       updateQuestion(2);
+      // Cargo las preguntas/ movies
+      optionsMovie(questions, moviesArray, leterOption);
+      console.log("quest 2 active");
       break;
     case "quest3":
-      // Obtener las preguntas por su ID
-      const question3 = document.getElementById("question3");
+      // Le quito la class active
+      question2.classList.remove("active");
       // Le agrego la class active
       question3.classList.add("active");
       updateQuestion(3);
+      // Cargo las preguntas/ movies
+      optionsMovie(questions, moviesArray, leterOption);
       break;
 
     default:
@@ -55,14 +72,22 @@ function formQuestions(questions, data) {
 }
 
 // Función para inicializar las imágenes
-function optionsMovie(questions, movies) {
+function optionsMovie(questions, movies, leterOption) {
   // Obtener las imágenes por su ID
   const movie1 = document.getElementById("movie1");
   const movie2 = document.getElementById("movie2");
   const movie3 = document.getElementById("movie3");
 
   // Obtener las películas correspondientes al grupo de preguntas
-  const moviesToLoad = movies[questions];
+  const moviesToLoad = movies[`${questions}_${leterOption}`];
+
+  // Verificar si moviesToLoad está definido
+  if (!moviesToLoad) {
+    console.error(
+      `No se encontraron películas para la clave: ${questions}_${leterOption}`
+    );
+    return;
+  }
 
   // Asignar las imágenes y textos alternativos a cada película
   movie1.src = moviesToLoad[0].img;
@@ -75,16 +100,45 @@ function optionsMovie(questions, movies) {
   movie3.alt = moviesToLoad[2].img_alt;
 }
 
-// Función para cambiar una película
-function changeMovie(index, movieData) {}
+// Función pasar al siguiente grupo de preguntas/ movies
+function nextGroup(idMovie) {
+  console.log("idMovie", idMovie);
 
-// Ejemplo de cómo cambiar una película (por ejemplo, al hacer click en una imagen):
-document.querySelectorAll(".movie-card img").forEach((img, index) => {
-  img.addEventListener("click", () => {
-    // Llamar a la función de cambiar imagen al hacer clic
-    changeMovie(index, {
-      img: "new-image-path.jpg", // Nueva ruta de la imagen
-      img_alt: "New Movie", // Nuevo texto del atributo alt
-    });
+  currentQuestion++;
+  console.log("currentQ", currentQuestion);
+
+  // Uso un switch case para cada pregunta, usando idMovie para resolver que grupo de asignar
+  switch (idMovie) {
+    case "movie1":
+      // Llamo a la siguiente pregunta
+      formQuestions(`quest${currentQuestion}`, "A");
+      console.log("idmovie, currentQuestion", idMovie, currentQuestion);
+
+      break;
+    case "movie2":
+      // Llamo a la siguiente pregunta
+      formQuestions(`quest${currentQuestion}`, "B");
+      console.log("idmovie, currentQuestion", idMovie, currentQuestion);
+      break;
+    case "movie3":
+      // Llamo a la siguiente pregunta
+      formQuestions(`quest${currentQuestion}`, "C");
+      break;
+    default:
+      // error por defecto
+      console.error("Error loading the movie data:", error);
+
+      break;
+  }
+}
+
+// Seleccionamos todas las imágenes dentro de .movie-card
+document.querySelectorAll(".movie-card img").forEach((img) => {
+  img.addEventListener("click", (event) => {
+    // Obtener el valor del atributo alt de la imagen clickeada
+    const movieAlt = event.target.id;
+
+    // Llamar a la función nextGroup con el valor del alt y el id actual de la imagen
+    nextGroup(movieAlt);
   });
 });
